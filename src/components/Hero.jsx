2c118722623cfbe0dd4dbc5784fe3fb2b3f6f9c0
rahulbatra-19 <batrarahul409@gmail.com>
2023-10-19 00:52:31 +0530

    Build navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
+const Hero = () => {
+  return <section className='relatibe w-full h-screen mx-auto'>
+    <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl flex mx-auto flex-row items-start gap-5`}>
+    <div className='flex'></div></div>Hero</section>;
+};
+
+export default Hero;
